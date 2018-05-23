2b0c0af318ce195b6438c43e5c993cef94ca3476
jimib <jimi.bailey@gmail.com>
2018-05-23 16:25:21 +0100

    Moved installation directory

--- build/scripts/install.js
+++ build/scripts/install.js
@@ -4,7 +4,7 @@ const util = require('util');
 const Promise = require('bluebird');
 const exec = require('child_process').exec;
 
-const PATH_MOCK_API = path.resolve( './_mockapi');
+const PATH_MOCK_API = path.resolve( '../../../_mockapi');
 
 const PATH_MOCK_API_SCHEMA = path.resolve( PATH_MOCK_API, './schema' );
 const PATH_MOCK_API_SCRIPTS = path.resolve( PATH_MOCK_API, './scripts' );
@@ -29,7 +29,7 @@ Promise.mapSeries([
 	() => ensureFileExists( PATH_MOCK_API_ROUTES, ( file ) => fs.copyAsync( path.resolve( PATH_BUILD, 'routes.json'), file ) ),
 	() => ensureDirExists( PATH_MOCK_API_STORE ),
 	() => addPackageScripts(),
-	() => ensureFileExists( PATH_MOCK_API_STORE_DB, () => execPromise( exec( 'npm run api:build', { cwd: path.resolve('.') } ) ) )
+	() => ensureFileExists( PATH_MOCK_API_STORE_DB, () => execPromise( exec( 'npm run api:build', { cwd: path.resolve('../../../') } ) ) )
 ], handler => handler() )
 .then( result => {
 	//all files are in place
