d291627f641dfc285f6fd8fadb4912df7f0b77e0
jimib <jimi.bailey@gmail.com>
2018-06-23 11:03:48 +0100

    Tidied up example of code - works better with dummy-json cache

--- build/schema/helpers.js
+++ build/schema/helpers.js
@@ -1,5 +1,13 @@
+const uuid = require('uuid');
+const dummyjson = require('dummy-json');
+const Handlebars = require('handlebars');
+
 module.exports = {
-	hello : () => {
-		return 'Hello world';
+	hello : ( options ) => {
+		const cache = options.data.root.__cache;
+		return new Handlebars.SafeString(`Hello '${cache.email_firstName || dummyjson.helpers.firstName(options)}'`);
+	},
+	uuid : ( item ) => {
+		return uuid();
 	}
 }
